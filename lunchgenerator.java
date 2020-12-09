2b14430757a6ed2439e18e4afc58687590e4814b
EddyTam <tamsiukei@yahoo.com.hk>
2020-12-09 13:59:09 +0800

    Update lunchgenerator.java

--- lunchgenerator.java
+++ lunchgenerator.java
@@ -2,15 +2,15 @@ package lunchgenerator;
 import javax.swing.*;
 public class Lunchgenerator {
 	public static void main(String[] agrs){
-		int i = 0;
+		int counter = 0;
 		int n = 0;
-		String [] a = {"八方雲集","Mcdonald","thai food","銀咖喱","醉瓊樓","KFC","太興","譽居","韓館","tam"};	
+		String [] resterant = {"八方雲集","Mcdonald","thai food","銀咖喱","醉瓊樓","KFC","太興","譽居","韓館","tam"};	
 		do{
 			do{
-				i = (int)(Math.random()*10);
-			}while(i>(a.length-1));
+				counter = (int)(Math.random()*10);
+			}while(counter>(resterant.length-1));
 			Object[] options ={ "OK", "Generate" };  
-			n = JOptionPane.showOptionDialog(null, a[i]+"\nGenerate again？", "Lunch Generator",JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
+			n = JOptionPane.showOptionDialog(null, resterant[counter]+"\nGenerate again？", "Lunch Generator",JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
 		}while(n!=0);
 	}	
 }
